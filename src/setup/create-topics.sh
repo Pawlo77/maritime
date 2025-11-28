1c487a509c4f762191adb1bbfbf13b68dec7d6f4
Pawel <pawflor@gmail.com>
2025-11-28 20:06:45 +0100

    feat: working everything but airflow

--- src/setup/create-topics.sh
+++ src/setup/create-topics.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-set -e
 
-TOPIC_NAME="ais-stream"
-PARTITIONS=3
-REPLICATION=1
-BOOTSTRAP_SERVER="kafka:29092"
+echo "Czekam na start Kafki..."
+# cub to narzedzie wbudowane w obrazy Confluent do sprawdzania dostepnosci uslug
+cub kafka-ready -b kafka:29092 1 60
 
-# Wait until Kafka is ready
-MAX_RETRIES=20
-RETRY=0
-until kafka-topics --bootstrap-server "$BOOTSTRAP_SERVER" --list >/dev/null 2>&1; do
-    RETRY=$((RETRY+1))
-    if [ "$RETRY" -gt "$MAX_RETRIES" ]; then
-        echo "Kafka broker did not start in time. Exiting."
-        exit 1
-    fi
-    echo "Waiting for Kafka ($RETRY/$MAX_RETRIES)..."
-    sleep 2
-done
+echo "Tworze temat 'ais-data'..."
+kafka-topics --create --if-not-exists --bootstrap-server kafka:29092 --partitions 1 --replication-factor 1 --topic ais-data
 
-# Create topic
-kafka-topics --bootstrap-server "$BOOTSTRAP_SERVER" \
---create \
---if-not-exists \
---topic "$TOPIC_NAME" \
---partitions "$PARTITIONS" \
---replication-factor "$REPLICATION"
-
-echo "Topic $TOPIC_NAME created successfully!"
-
-# list topics to verify
-echo "Listing all topics:"
-kafka-topics --bootstrap-server "$BOOTSTRAP_SERVER" --list
+echo "Lista tematow:"
+kafka-topics --list --bootstrap-server kafka:29092
